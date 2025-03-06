2b8bd77de154511cc6f67223f750c690a293d084
Joe <active.rain.drop@gmail.com>
2025-03-06 03:10:12 +0100

    production ready

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -13,7 +13,7 @@ export const API_ENDPOINTS = {
   },
 } as const;
 export const IS_LOCAL = false;
-export const IS_DEV = true;
+export const IS_DEV = false;
 export const API_BASE_URL = IS_DEV
   ? `https://dev.${import.meta.env.VITE_BASE_DOMAIN}/api` // falls IS_DEV true ist, verwende die Entwicklungs-API
   : `https://${import.meta.env.VITE_BASE_DOMAIN}/api`; // Verwende die Produktions-API
